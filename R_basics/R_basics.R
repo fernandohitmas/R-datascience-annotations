ba9544dee41fc50610a20d2182b7b19f867f5b53
fernandohitmas <fernando.masumoto@gmail.com>
2025-01-11 20:49:43 -0300

    List of arithmetic operations in R

---
+++ R_basics/R_basics.R
@@ -0,0 +1,11 @@
+## Basic arithmetics
+
+# sum : +
+# subtraction : -
+# multiplication : *
+# division : /
+# power : ^
+# remainder of a division: %%
+# operations follow the mathetical order, 
+# which includes the parenthesis logic
+

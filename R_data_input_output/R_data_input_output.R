aa2a51b9202cbfd98c2347230564ee8f821f9f4a
fernandohitmas <fernando.masumoto@gmail.com>
2025-01-16 21:40:26 -0300

    input and output with R

---
+++ R_data_input_output/R_data_input_output.R
@@ -0,0 +1,23 @@
+# write.csv(object, file = name) - to create a csv file from a variable
+# read.csv(file = name) - to read a csv file
+
+# READXL and WRITEXL from tidyverse
+# install.packages('readxl')
+# library('readxl')
+
+# SQL with R 
+# The most common library to work with databases
+# 1 - usually the connection needs the database name, de user id and a password
+# 2 - connect to a table
+# 3 - make a query
+# install.packages('RODBC')
+# library(RODBC)
+# MYSQL - RMySQL
+# Oracle - ROracle
+# JDBC - RJDBC
+
+# WEB SCRAPING with R
+# install.packages('rvest')
+# library(rvest)
+demo(package = 'rvest')
+demo(package = 'rvest', topic = 'tripadvisor')

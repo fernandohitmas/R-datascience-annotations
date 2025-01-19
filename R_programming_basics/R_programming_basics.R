c356ea8e3e9f1caeba61b2d5ef5719b378ac8a8d
fernandohitmas <fernando.masumoto@gmail.com>
2025-01-18 21:54:27 -0300

    Loops and Functions

--- R_programming_basics/R_programming_basics.R
+++ R_programming_basics/R_programming_basics.R
@@ -19,3 +19,28 @@ if (x > 10){
 }else {
    print('X is 10!')
 }
+
+# WHILE loops
+# while (condition){code}
+n <- 0
+
+while (n < 5){
+    print(n)
+
+    if (n == 3){
+        print("N reached 3")
+        break
+    }
+
+    n <- n + 1
+}
+
+# FOR Loops
+# for (var in vector/list/matrix) {code}
+# For loop iterates through a matrix by column
+
+# FUNCTIONS
+# func1 <- function(arg1,arg2,arg3=0){
+#     code
+#     return(something)
+# }

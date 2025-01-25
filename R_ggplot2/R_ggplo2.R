22faa5e947427305076f0d9201f0f4529fe3f82e
fernandohitmas <fernando.masumoto@gmail.com>
2025-01-25 15:52:15 -0300

    Histograms and scatterplots

--- R_ggplot2/R_ggplo2.R
+++ R_ggplot2/R_ggplo2.R
@@ -1,6 +1,7 @@
 # install.packages('ggplot2movies')
 library(ggplot2)
 library(ggplot2movies)
+library(dplyr)
 
 ### HISTOGRAMS
 # data and arsthetics
@@ -17,6 +18,25 @@ pl2 <- pl + geom_histogram(
 
 pl3 <- pl2 + 
     xlab("Movie Rating") +
-    ylab("Count")
+    ylab("Count") +
+    ggtitle("Movies rating histogram")
+
+pl + geom_histogram(bins=50, aes(fill=after_stat(count)))
 
 
+### Scatterplot
+df <- mtcars
+
+# DATA & AESTHETICS LAYER
+pl1 <- df %>% ggplot(aes(x=wt, y=mpg))
+
+# GEOMETRY
+# size = 5
+# pl1 + geom_point(size=5, alpha=0.3)
+pl1 + geom_point(
+    aes(
+        shape=factor(cyl),
+        color=factor(cyl)),
+    size=5)
+pl2 <- pl1 + geom_point(aes(color=hp), size=5)
+pl2 + scale_color_gradient(low="blue", high="red")
